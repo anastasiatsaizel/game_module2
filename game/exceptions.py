56931d8d71eefec6fb63b40b9024d0585662c637
Anastasia <nastya@MacBook-Air-Nasta.local>
2026-06-22 17:49:29 +0200

    Clean main branch for PR

--- game/exceptions.py
+++
@@ -1,5 +0,0 @@
-class InvalidInputError(Exception):
-    pass
-
-class InvalidRollError(Exception):
-    pass
--- game/models.py
+++
@@ -1,9 +0,0 @@
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-
-class Computer:
-    def __init__(self):
-        self.name = "Computer"
-        self.score = 0
